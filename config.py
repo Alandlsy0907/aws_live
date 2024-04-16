2b68fc58949c5a41170f528b74d57571f6924bb8
Alandlsy0907 <115888720+Alandlsy0907@users.noreply.github.com>
2024-04-16 14:02:16 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cvyie04s88c9.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "database-2"
+custombucket = "alandlimshengyang-employee"
+customregion = "us-east-1"
+
